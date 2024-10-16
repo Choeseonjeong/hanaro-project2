77c0e861469bf682347f0868c336c635bfea1ae8
Choeseongeong <tjswjd7166@gmail.com>
2024-10-16 18:43:46 +0900

    헤더

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
 import LoginButton from "../ui/LoginButton";
 
 export default function Header({
@@ -7,37 +8,34 @@ export default function Header({
 }: {
   onShowAddRecipe: () => void;
 }) {
+  const { data: session } = useSession();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    const loggedInUser = localStorage.getItem("loggedInUser");
-    if (loggedInUser) {
-      setIsLoggedIn(true);
-    } else {
-      setIsLoggedIn(false);
-    }
-  }, []);
+    const localLoggedInUser = localStorage.getItem("loggedInUser");
+    setIsLoggedIn(!!localLoggedInUser || !!session);
+  }, [session]);
 
   const handleAddRecipe = () => {
     onShowAddRecipe();
   };
 
   return (
-    <>
-      <header className="flex justify-between items-center p-4">
-        <button className="text-4xl font-bold font-serif text-black">
-          My Recipe
-        </button>
-        <div>
+    <header className="flex justify-between items-center p-4">
+      <button className="text-4xl font-bold font-serif text-black">
+        My Recipe
+      </button>
+      <div className="flex items-center">
+        {isLoggedIn && (
           <button
             className="mr-4 text-gray-500 px-4 py-2 font-bold hover:text-my-color2"
             onClick={handleAddRecipe}
           >
             레시피 추가
           </button>
-          <LoginButton />
-        </div>
-      </header>
-    </>
+        )}
+        <LoginButton />
+      </div>
+    </header>
   );
 }

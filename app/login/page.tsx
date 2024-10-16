c23f8470f89ece122fbb3bd99c6002aeb4a27fcd
Choeseongeong <tjswjd7166@gmail.com>
2024-10-16 18:29:46 +0900

    w

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,7 +10,8 @@ export default function LoginPage() {
   const [password, setPassword] = useState<string>("");
   const [errorMessage, setErrorMessage] = useState<string>("");
   const router = useRouter();
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
+  const isLoading = status === "loading";
 
   useEffect(() => {
     if (session?.user?.email) {
@@ -30,7 +31,9 @@ export default function LoginPage() {
       } else {
         alert("로그인 성공!");
       }
-      router.push("/");
+      setTimeout(() => {
+        router.push("/");
+      }, 500);
     }
   }, [session, router]);
 
@@ -64,62 +67,68 @@ export default function LoginPage() {
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
-        <h1 className="text-4xl font-bold mb-6 grid place-items-center">
-          Login
-        </h1>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block text-gray-700 mb-2 font-bold">이메일</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="your-email@example.com"
-              className="w-full px-4 py-2 border rounded-md"
-              required
-            />
+      {isLoading ? (
+        <p>로딩중</p>
+      ) : (
+        <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+          <h1 className="text-4xl font-bold mb-6 grid place-items-center">
+            Login
+          </h1>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2 font-bold">
+                이메일
+              </label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="your-email@example.com"
+                className="w-full px-4 py-2 border rounded-md"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2 font-bold">
+                비밀번호
+              </label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="비밀번호를 입력하세요"
+                className="w-full px-4 py-2 border rounded-md font-bold"
+                required
+              />
+            </div>
+            {errorMessage && (
+              <p className="text-red-500 font-bold mb-4">{errorMessage}</p>
+            )}
+            <button
+              type="submit"
+              className="w-full bg-my-color text-white py-2 rounded-md font-bold hover:bg-my-color2"
+            >
+              로그인
+            </button>
+          </form>
+          <div className="mt-4">
+            <button
+              className="w-full bg-white border border-gray-300 text-gray-700 py-2 rounded-md flex justify-center items-center hover:bg-gray-100"
+              onClick={() => signIn("google", { callbackUrl: "/" })}
+            >
+              <FcGoogle />
+              &nbsp; Sign in with Google
+            </button>
+            <button
+              className="w-full bg-white border border-gray-300 text-gray-700 py-2 rounded-md flex justify-center items-center mt-2 hover:bg-gray-100"
+              onClick={() => signIn("github", { callbackUrl: "/" })}
+            >
+              <FaGithub />
+              &nbsp; Sign in with GitHub
+            </button>
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 mb-2 font-bold">
-              비밀번호
-            </label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="비밀번호를 입력하세요"
-              className="w-full px-4 py-2 border rounded-md font-bold"
-              required
-            />
-          </div>
-          {errorMessage && (
-            <p className="text-red-500 font-bold mb-4">{errorMessage}</p>
-          )}
-          <button
-            type="submit"
-            className="w-full bg-my-color text-white py-2 rounded-md font-bold hover:bg-my-color2"
-          >
-            로그인
-          </button>
-        </form>
-        <div className="mt-4">
-          <button
-            className="w-full bg-white border border-gray-300 text-gray-700 py-2 rounded-md flex justify-center items-center hover:bg-gray-100"
-            onClick={() => signIn("google", { callbackUrl: "/" })}
-          >
-            <FcGoogle />
-            &nbsp; Sign in with Google
-          </button>
-          <button
-            className="w-full bg-white border border-gray-300 text-gray-700 py-2 rounded-md flex justify-center items-center mt-2 hover:bg-gray-100"
-            onClick={() => signIn("github", { callbackUrl: "/" })}
-          >
-            <FaGithub />
-            &nbsp; Sign in with GitHub
-          </button>
         </div>
-      </div>
+      )}
     </div>
   );
 }
